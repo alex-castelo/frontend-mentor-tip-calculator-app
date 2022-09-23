ddfbb6b01a28a3bfb61f7c1968adde5d6933951d
alex.castelo <alexcastelomartinez@gmail.com>
2022-09-23 16:09:15 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <h1>Hello</h1>
+    <body>
+      Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
+      person Total / person Reset
+      <p>
+        <base-input label="Bill" v-model="bill" type="number" icon="dollar" />
+      </p>
+
+      <div class="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+          >Frontend Mentor</a
+        >. Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </body>
+  </main>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import BaseInput from './components/BaseInput.vue'
+
+const bill = ref('')
+</script>
+<style scoped>
+.attribution {
+  font-size: 11px;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,90 @@
+<template>
+  <label class="base-input">
+    <div class="input-feedback">
+      <span class="input-label"> {{ label }}</span>
+      <span class="input-error">Can't be zero</span>
+    </div>
+    <div class="input-wrapper">
+      <img
+        v-if="icon !== 'none'"
+        :src="`./src/assets/icons/icon-${icon}.svg`"
+        class="input-icon"
+      />
+      <input
+        v-bind="$attrs"
+        :placeholder="label"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event?.target?.value)"
+      />
+    </div>
+  </label>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BaseInput',
+}
+</script>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+type InputIcons = 'person' | 'dollar' | 'none'
+
+defineProps({
+  label: { type: String, default: '' },
+  modelValue: { type: [String, Number], default: '' },
+  icon: { type: String as PropType<InputIcons>, default: 'none' },
+})
+</script>
+
+<style scoped>
+.base-input {
+  display: grid;
+  row-gap: 0.25rem;
+  grid-template-columns: 1fr;
+  max-width: fit-content;
+}
+
+.input-feedback {
+  display: flex;
+  justify-content: space-between;
+}
+
+input {
+  font-size: 24px;
+  background-color: transparent;
+  color: var(--color-cyan-dark);
+  border: none;
+  text-align: right;
+  font-weight: 700;
+  padding: 8px;
+}
+
+.input-wrapper:focus-visible,
+.input-wrapper:hover,
+.input-wrapper:focus,
+.input-wrapper:active {
+  border: 2px solid var(--color-gray-dark);
+}
+
+.input-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  background-color: var(--color-gray-lighter);
+  border-radius: 8px;
+  margin: none;
+}
+
+.input-icon {
+  margin: 0 8px;
+  width: 16px;
+  height: 16px;
+}
+
+.input-info {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
