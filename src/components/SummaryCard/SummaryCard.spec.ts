2d9350d057362a81431f1ff380d356311079f72f
alex.castelo <alexcastelomartinez@gmail.com>
2022-10-06 20:53:40 +0200

    Add input disabled test

--- src/components/SummaryCard/SummaryCard.spec.ts
+++ src/components/SummaryCard/SummaryCard.spec.ts
@@ -17,23 +17,37 @@ describe('BaseCard', () => {
     const totalSummary = wrapper.findAll('.summary-item')[1]
     const resetButton = wrapper.find('button')
 
-    expect(personSummary.find('.amount').text()).toBe(`$${PERSON_AMOUNT}`)
+    expect(personSummary.find('.amount').text()).toBe(`$${PERSON_AMOUNT}.00`)
     expect(personSummary.find('.title').text()).toBe('Tip Amount')
     expect(personSummary.find('.subtitle').text()).toBe('/ person')
 
-    expect(totalSummary.find('.amount').text()).toBe(`$${TOTAL_AMOUNT}`)
+    expect(totalSummary.find('.amount').text()).toBe(`$${TOTAL_AMOUNT}.00`)
     expect(totalSummary.find('.title').text()).toBe('Total')
     expect(totalSummary.find('.subtitle').text()).toBe('/ person')
 
     expect(resetButton.text()).toBe('RESET')
   })
 
-  it('emitts reset event on button click', () => {
+  it('shows input disabled in empty state', async () => {
     const wrapper = mount(SummaryCard)
 
     const resetButton = wrapper.find('button')
 
-    resetButton.trigger('click')
+    expect(resetButton.attributes().disabled).toBeDefined()
+    expect(resetButton.element.disabled).toBe(true)
+  })
+
+  it('emitts reset event on button click', async () => {
+    const wrapper = mount(SummaryCard, {
+      props: {
+        personAmount: 1,
+        totalAmount: 1,
+      },
+    })
+
+    const resetButton = wrapper.find('button')
+
+    await resetButton.trigger('click')
 
     expect(wrapper.emitted()).toHaveProperty('reset')
   })
