f398b7fcdf2ddb3817d8a592c0da577eeedd467a
Àlex Castelo Martínez <alexcastelomartinez@gmail.com>
2022-10-10 21:24:17 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend-mentor-tip-calculator-app/",
   plugins: [vue()],
   test: {
     globals: true,
