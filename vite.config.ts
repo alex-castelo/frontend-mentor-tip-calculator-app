a387f5e052b3dd360a1e569cae041fa9b263bf2e
Àlex Castelo Martínez <alexcastelomartinez@gmail.com>
2023-09-02 11:48:39 +0200

    run eslint

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend-mentor-tip-calculator-app/",
+  base: '/frontend-mentor-tip-calculator-app/',
   plugins: [vue()],
   test: {
     globals: true,
